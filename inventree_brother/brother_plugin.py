6fcc568add91aeb1f30d9bf48b3b2893e03ee8b9
Eva van Mosseveld <eva.van.mosseveld@sensorfy.ai>
2023-06-01 12:47:59 +0200

    Get required image dimensions from label type specifications. Refactored a bit.

--- inventree_brother/brother_plugin.py
+++ inventree_brother/brother_plugin.py
@@ -106,30 +106,39 @@ class BrotherLabelPlugin(LabelPrintingMixin, SettingsMixin, InvenTreePlugin):
         """
 
         # TODO: Add padding around the provided image, otherwise the label does not print correctly
+        # ^ Why? The wording in the underlying brother_ql library ('dots_printable') seems to suggest
+        # at least that area is fully printable.
         # TODO: Improve label auto-scaling based on provided width and height information
 
         # Extract width (x) and height (y) information
-        width = int(kwargs['width'])
-        height = int(kwargs['height'])
+        # width = kwargs['width']
+        # height = kwargs['height']
+        # ^ currently this width and height are those of the label template (before conversion to PDF
+        # and PNG) and are of little use
 
         # Extract image from the provided kwargs
         label_image = kwargs['png_file']
 
-        # Create an empty canvas with the correct size needed for the label type
-        label_size = (306, 991)  # (width, height)
-        canvas = Image.new("RGB", label_size, "white")
-        # Paste the label image centered on the canvas
-        image_offset = ((label_size[0] - label_image.width) // 2,
-                        (label_size[1] - label_image.height) // 2)
-        canvas.paste(label_image, image_offset)
-
         # Read settings
         model = self.get_setting('MODEL')
         ip_address = self.get_setting('IP_ADDRESS')
-        label = self.get_setting('LABEL')
+        media_type = self.get_setting('LABEL')
+
+        # As the brother_ql library only accepts an image with exactly the right size for the label type,
+        # we need to resize the image. For now just center the image with some whitespace around the side,
+        # but ideally this plugin or maybe the label printing base module should provide some formatting
+        # options for scaling, margins, etc.
+        # TODO: correctly handle case where label image is bigger than printable area
+        for media_specifications in ALL_LABELS:
+            if media_specifications.identifier == media_type:
+                printable_dimensions = media_specifications.dots_printable
+        printable_image = Image.new("RGB", printable_dimensions, "white")
+        margin_left = (printable_dimensions[0] - label_image.width) // 2
+        margin_top = (printable_dimensions[1] - label_image.height) // 2
+        printable_image.paste(label_image, (margin_left, margin_top))
 
         # Check if red labels used
-        if label in ['62red']:
+        if media_type in ['62red']:
             red = True
         else:
             red = False
@@ -139,8 +148,8 @@ class BrotherLabelPlugin(LabelPrintingMixin, SettingsMixin, InvenTreePlugin):
         # Generate instructions for printing
         params = {
             'qlr': printer,
-            'images': [canvas],
-            'label': label,
+            'images': [printable_image],
+            'label': media_type,
             'cut': self.get_setting('AUTO_CUT'),
             'rotate': self.get_setting('ROTATION'),
             'compress': self.get_setting('COMPRESSION'),
